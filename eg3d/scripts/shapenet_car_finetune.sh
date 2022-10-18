01fbd5214e0b667d36bf38763d89b3df4faa5673
mengxuyi <mengxuyiabc@gmail.com>
2022-10-17 22:22:13 -0700

    added completed dataset to script

--- eg3d/scripts/shapenet_car_finetune.sh
+++ eg3d/scripts/shapenet_car_finetune.sh
@@ -26,7 +26,8 @@ BASE_DIR=/home/xuyi/Repo/eg3d
 
 
 ##---------abo with 3D volume + no pretraining (because feature channel is down to 8)-----------
-DATA=${BASE_DIR}/dataset_preprocessing/abo/abo_128_copy.zip
+# DATA=${BASE_DIR}/dataset_preprocessing/abo/abo_128_copy.zip
+DATA=${BASE_DIR}/dataset_preprocessing/abo/abo_128_completed.zip
 # DATA=${BASE_DIR}/dataset_preprocessing/shapenet_cars/cars_128_copy.zip
 BATCH_SIZE=1
 python train.py --outdir=${BASE_DIR}/try-runs --cfg=abo_dataset --data=${DATA} \
